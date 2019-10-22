6e9431a036d13701ecceed47f99d9beeecfd183d
Carmeli Tamir <carmeli.tamir@gmail.com>
2019-10-21 22:59:47 -0400

    Parseing return value

--- kernel/skunk_device.c
+++ kernel/skunk_device.c
@@ -19,9 +19,13 @@ static int release_skunk_device(struct inode *inodep, struct file *filep)
 
 static long ioctl_skunk_device(struct file *file, unsigned int cmd, unsigned long arg)
 {
+    Skunk__ReturnValue skunk_ret;
     long ret = 0;
     u32 message_size;
-    char* message;
+    u32 ret_message_size;
+    char* message = NULL;
+
+    skunk__return_value__init(&skunk_ret);
 
     switch (cmd) {
     case CALL_FUNCTION:
@@ -33,14 +37,38 @@ static long ioctl_skunk_device(struct file *file, unsigned int cmd, unsigned lon
             return -ENOMEM;
         }
         if (copy_from_user(message, ((char*)arg ) + sizeof(message_size), message_size)) {
-            kfree(message);
-            return -ENOMEM;
+            ret = -ENOMEM;
+            goto out;
         }
-        ret = parse_user_buffer_and_call_function(message, message_size);
-        kfree(message);
+
+        if (0 == parse_user_buffer_and_call_function(message, message_size, &skunk_ret)) {
+            // Pack return value back to buffer
+            ret_message_size = skunk__return_value__get_packed_size(&skunk_ret);
+            if (ret_message_size > message_size) {
+                pr_info("Buffer is too small");
+                ret = -ENOMEM;
+                goto out;
+            }
+            skunk__return_value__pack(&skunk_ret, message);
+            if (copy_to_user((void*)arg, (void *)&ret_message_size, sizeof(ret_message_size))) {
+                ret = -ENOMEM;
+                goto out;
+            }
+            if (copy_to_user(((void*)arg ) + sizeof(message_size), message, ret_message_size)) {
+                kfree(message);
+                ret = -ENOMEM;
+                goto out;
+            }
+        }
+
         break;
     default:
         ret = -EINVAL;
+    }
+
+out:
+    if (message) {
+        kfree(message);
     }
     return ret;
 }
