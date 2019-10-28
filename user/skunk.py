0a5ac11ccc11fdd900830210aec02468325485f5
Carmeli Tamir <carmeli.tamir@gmail.com>
2019-10-27 20:11:10 -0400

    Refactor towards auto code generation

--- user/skunk.py
+++ user/skunk.py
@@ -7,13 +7,10 @@ import skunk_pb2
 def binary_length_and_value(buffer, buffer_length):
     return struct.pack('I', buffer_length) + buffer
 
-def call_function_one_arg(device, fname, fargs, fret, arg1, type1):
-    func_type = skunk_pb2.function_type()
-    func_type.args = fargs
-    func_type.ret = fret
-    func_type_binary = binary_length_and_value(func_type.SerializeToString(), func_type.ByteSize())
-
-    func_with_one_arg = skunk_pb2.func_with_1_arg()
+def call_function_one_arg(device, fname, num_args, fret, arg1, type1):
+    func_with_one_arg = skunk_pb2.FunctionCall()
+    func_with_one_arg.returnType = fret
+    func_with_one_arg.numberOfArguments = num_args
     func_with_one_arg.name = fname
     func_with_one_arg.arg1.type = type1
 
@@ -26,9 +23,7 @@ def call_function_one_arg(device, fname, fargs, fret, arg1, type1):
 
     func_with_one_arg_binary = binary_length_and_value(func_with_one_arg.SerializeToString(), func_with_one_arg.ByteSize())
 
-    iotcl_args = binary_length_and_value(func_type_binary + func_with_one_arg_binary, len(func_type_binary + func_with_one_arg_binary))
-
-    call_result = fcntl.ioctl(device, 0xc008ee00, iotcl_args)
+    call_result = fcntl.ioctl(device, 0xc008ee00, func_with_one_arg_binary)
 
     length_of_size_field = struct.calcsize('I')
     return_length, = struct.unpack('I', call_result[:length_of_size_field])
@@ -42,13 +37,13 @@ def call_function_one_arg(device, fname, fargs, fret, arg1, type1):
 def run_skunk():
     with open("/dev/skunk", 'r') as skunk_device:
         ret = call_function_one_arg(skunk_device, "kallsyms_lookup_name",
-        skunk_pb2.function_type.oneArg, skunk_pb2.function_type.eightByte,
+        1, skunk_pb2.FunctionCall.eightByte,
         "kallsyms_lookup_name", skunk_pb2.Argument.stringArg)
 
         print("Got adress of {}".format(hex(2**64 + ret.ret64))) # Printing 2's complement of the address
 
         ret = call_function_one_arg(skunk_device, "round_jiffies",
-        skunk_pb2.function_type.oneArg, skunk_pb2.function_type.eightByte,
+        1, skunk_pb2.FunctionCall.eightByte,
         133713371337, skunk_pb2.Argument.eightByteArg)
 
         print("Got result of {}".format(ret.ret64))
