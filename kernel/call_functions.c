425c42b587a0f9d99194d8ecacd062fc2f2de33a
Carmeli Tamir <carmeli.tamir@gmail.com>
2019-11-01 18:44:55 -0400

    Small rename

--- kernel/call_functions.c
+++ kernel/call_functions.c
@@ -5,7 +5,7 @@
 
 #include "skunk.pb-c.h"
 
-static void parse_proto_and_call_function_ret_64_oneArg(char *name, Skunk__Argument * arg1, Skunk__ReturnValue *ret)
+static void call_function_oneArg(char *name, Skunk__Argument * arg1, Skunk__ReturnValue *ret)
 {
     unsigned long func_addr;
 
@@ -31,7 +31,7 @@ static void parse_proto_and_call_function_ret_64_oneArg(char *name, Skunk__Argum
     ret->has_ret64 = 1;
 }
 
-static void parse_proto_and_call_function_ret_64_twoArg(char *name, Skunk__Argument * arg1, Skunk__Argument * arg2, Skunk__ReturnValue *ret)
+static void call_function_twoArg(char *name, Skunk__Argument * arg1, Skunk__Argument * arg2, Skunk__ReturnValue *ret)
 {
     pr_info("Hello Function two args");
 }
@@ -44,20 +44,14 @@ long parse_user_buffer_and_call_function(char *buffer, u32 length, Skunk__Return
     if (NULL == func_call) {
         return -EINVAL;
     }
-    
-    if (func_call->returntype != SKUNK__FUNCTION_CALL__RETURN_TYPE__eightByte) {
-        pr_info("Currently supporting only 8 bytes return value");
-        skunk__function_call__free_unpacked(func_call, NULL);
-        return -EINVAL;
-    }
 
     switch (func_call->numberofarguments)
     {
         case 1:
-            parse_proto_and_call_function_ret_64_oneArg(func_call->name, func_call->arg1, ret);
+            call_function_oneArg(func_call->name, func_call->arg1, ret);
         break;
         case 2:
-            parse_proto_and_call_function_ret_64_twoArg(func_call->name, func_call->arg1, func_call->arg2, ret);
+            call_function_twoArg(func_call->name, func_call->arg1, func_call->arg2, ret);
         break;
     default:
         break;
