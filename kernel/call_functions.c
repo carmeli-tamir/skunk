db0fd216d14cba6a9310daca57056e9e767e87e0
Carmeli Tamir <carmeli.tamir@gmail.com>
2019-11-02 19:58:21 -0400

    Using ID function

--- kernel/call_functions.c
+++ kernel/call_functions.c
@@ -5,7 +5,34 @@
 
 #include "skunk.pb-c.h"
 
-static void call_function_oneArg(char *name, Skunk__Argument * arg1, Skunk__ReturnValue *ret)
+u32 function_call_id(Skunk__FunctionCall__ReturnType ret, u8 numOfArguments,
+                     Skunk__Argument__ArgumentType arg1, Skunk__Argument__ArgumentType arg2,
+                     Skunk__Argument__ArgumentType arg3, Skunk__Argument__ArgumentType arg4,
+                     Skunk__Argument__ArgumentType arg5, Skunk__Argument__ArgumentType arg6,
+                     Skunk__Argument__ArgumentType arg7)
+{
+    u32 id = 0;
+    if (ret >= 0x10 || numOfArguments >= 8 ||
+        arg1 >= 0x10 || arg2 >= 0x10 || arg3 >= 0x10 || arg4 >= 0x10 || arg5 >= 0x10 ||
+        arg6 >= 0x10 || arg7 >= 0x10 ) {
+            pr_info("Skunk got illegal function call id");
+            return INT_MAX;
+        }
+
+    id = ret << 28;
+    id |= numOfArguments >= 1 ? arg1 << 24 : 0;
+    id |= numOfArguments >= 2 ? arg2 << 20 : 0;
+    id |= numOfArguments >= 3 ? arg3 << 16 : 0;
+    id |= numOfArguments >= 4 ? arg4 << 12 : 0;
+    id |= numOfArguments >= 5 ? arg5 << 8 : 0;
+    id |= numOfArguments >= 6 ? arg6 << 4 : 0;
+    id |= numOfArguments >= 7 ? arg7 << 0 : 0;
+
+    return id;
+}
+
+static void call_function_oneArg(char *name, Skunk__FunctionCall__ReturnType ret_type,
+                                Skunk__Argument * arg1, Skunk__ReturnValue *ret)
 {
     unsigned long func_addr;
 
@@ -14,13 +41,13 @@ static void call_function_oneArg(char *name, Skunk__Argument * arg1, Skunk__Retu
         ret->status = SKUNK__RETURN_VALUE__CALL_STATUS__FunctionDoesntExist;
     }
 
-    switch (arg1->type)
+    switch (function_call_id(ret_type, 1, arg1->type, 0, 0, 0, 0, 0, 0))
     {
-    case SKUNK__ARGUMENT__ARGUMENT_TYPE__stringArg:
+    case 0x01000000:
         ret->ret64 = (int64_t)((ptrRet64StrArg)func_addr)(arg1->argstring);
         break;
 
-    case SKUNK__ARGUMENT__ARGUMENT_TYPE__eightByteArg:
+    case 0x00000000:
         ret->ret64 = (int64_t)((ptrRet64Int8bArg)func_addr)(arg1->argint8b);
         break;
 
@@ -31,7 +58,8 @@ static void call_function_oneArg(char *name, Skunk__Argument * arg1, Skunk__Retu
     ret->has_ret64 = 1;
 }
 
-static void call_function_twoArg(char *name, Skunk__Argument * arg1, Skunk__Argument * arg2, Skunk__ReturnValue *ret)
+static void call_function_twoArg(char *name, Skunk__FunctionCall__ReturnType ret_type,
+                                Skunk__Argument * arg1, Skunk__Argument * arg2, Skunk__ReturnValue *ret)
 {
     pr_info("Hello Function two args");
 }
@@ -48,10 +76,10 @@ long parse_user_buffer_and_call_function(char *buffer, u32 length, Skunk__Return
     switch (func_call->numberofarguments)
     {
         case 1:
-            call_function_oneArg(func_call->name, func_call->arg1, ret);
+            call_function_oneArg(func_call->name, func_call->returntype, func_call->arg1, ret);
         break;
         case 2:
-            call_function_twoArg(func_call->name, func_call->arg1, func_call->arg2, ret);
+            call_function_twoArg(func_call->name, func_call->returntype, func_call->arg1, func_call->arg2, ret);
         break;
     default:
         break;
