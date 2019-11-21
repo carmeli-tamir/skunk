aebea3268888f04d72dfb99ca73b60567ff93746
Carmeli Tamir <carmeli.tamir@gmail.com>
2019-11-21 15:27:38 -0500

    Upgrade python protobuf

--- user/setup.py
+++ user/setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='skunk', version='0.1', packages=find_packages(), install_requires=["protobuf==3.0.0"])
+setup(name='skunk', version='0.1', packages=find_packages(), install_requires=["protobuf==3.10.0"])
